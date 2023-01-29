fc390af4850d5ec6776c9d6fe9806caa997db3b1
Deeptendu Santra <deeptendu.santra@protonmail.com>
2023-01-30 03:01:45 +0530

    Reformat file

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from PIL import Image
 import gradio as gr
 from PIL import Image
@@ -6,13 +5,18 @@ from src import ClipVitB32, search
 
 MODEL = ClipVitB32()
 
-def get_matching_images(uploads:list, query:str):
+
+def get_matching_images(uploads: list, query: str):
     images = [Image.open(f.name) for f in uploads]
     img_embed = MODEL._gen_img_batch_encoding(images)
     hits = search(MODEL, query, img_embed)
     idx, scores = [f["corpus_id"] for f in hits], [f["score"] for f in hits]
     res_images = [uploads[i] for i in idx]
-    return {i: {"img": img.name, "score": score} for i, (img, score) in enumerate(zip(res_images, scores))}
+    return {
+        i: {"img": img.name, "score": score}
+        for i, (img, score) in enumerate(zip(res_images, scores))
+    }
+
 
 flagging_options = [
     "This is the image I was searching for.",
@@ -23,11 +27,13 @@ flagging_options = [
 ANS_DICT = {}
 ANS_POS = 0
 
+
 def prev_out():
     global ANS_POS, ANS_DICT
     ANS_POS = (ANS_POS - 1) % len(ANS_DICT)
     ans = ANS_DICT[ANS_POS]
     return ans["img"], ans["score"]
+
 
 def next_out():
     global ANS_POS, ANS_DICT
@@ -35,14 +41,20 @@ def next_out():
     ans = ANS_DICT[ANS_POS]
     return ans["img"], ans["score"]
 
+
 with gr.Blocks() as demo:
     gr.Markdown("Upload the images and the query that you are searching for.")
     with gr.Row():
         with gr.Column():
             file_input = gr.File(
-                file_count="multiple", file_types=["image"], label="Images", interactive=True,
+                file_count="multiple",
+                file_types=["image"],
+                label="Images",
+                interactive=True,
             )
-            text_input = gr.Textbox(label="Query", placeholder="Type your query here...")
+            text_input = gr.Textbox(
+                label="Query", placeholder="Type your query here..."
+            )
             submit_btn = gr.Button("Submit")
             clear_btn = gr.Button("Clear")
 
@@ -55,29 +67,43 @@ with gr.Blocks() as demo:
             # TODO: Add a js function to download current image
             download_btn = gr.Button("Download", visible=False)
 
-        def infer_and_display(images:list, query:str):
+        def infer_and_display(images: list, query: str):
             global ANS_DICT
             ANS_DICT = get_matching_images(images, query)
             return {
-                    matched_img: gr.update(value = ANS_DICT[0]["img"]),
-                    similarity_score: gr.update(value = ANS_DICT[0]["score"]),
-                    download_btn: gr.update(visible=True),
-                    }
+                matched_img: gr.update(value=ANS_DICT[0]["img"]),
+                similarity_score: gr.update(value=ANS_DICT[0]["score"]),
+                download_btn: gr.update(visible=True),
+            }
 
         def clear_input_and_output():
             global ANS_DICT, ANS_POS
             ANS_DICT = []
             ANS_POS = 0
             return {
-                    file_input: gr.update(value=None),
-                    text_input: gr.update(value=""),
-                    matched_img: gr.update(value=None),
-                    similarity_score: gr.update(value=None),
-                    download_btn: gr.update(visible=False),
-                    }
+                file_input: gr.update(value=None),
+                text_input: gr.update(value=""),
+                matched_img: gr.update(value=None),
+                similarity_score: gr.update(value=None),
+                download_btn: gr.update(visible=False),
+            }
 
-        submit_btn.click(infer_and_display, inputs=[file_input, text_input], outputs=[matched_img, similarity_score, download_btn])
-        clear_btn.click(clear_input_and_output, inputs=[], outputs=[text_input, file_input, matched_img, similarity_score, download_btn])
+        submit_btn.click(
+            infer_and_display,
+            inputs=[file_input, text_input],
+            outputs=[matched_img, similarity_score, download_btn],
+        )
+        clear_btn.click(
+            clear_input_and_output,
+            inputs=[],
+            outputs=[
+                text_input,
+                file_input,
+                matched_img,
+                similarity_score,
+                download_btn,
+            ],
+        )
 
         prev_btn.click(fn=prev_out, inputs=[], outputs=[matched_img, similarity_score])
         next_btn.click(fn=next_out, inputs=[], outputs=[matched_img, similarity_score])
